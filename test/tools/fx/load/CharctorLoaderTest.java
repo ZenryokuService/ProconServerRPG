9ec076b6c34771c46c2649379df2d9b06256c93d
takk <yogotak45@gmail.com>
2020-04-27 21:57:43 +0900

    キャラクター描画クラス追加

---
+++ test/tools/fx/load/CharctorLoaderTest.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2019-present ProconServerRPG JavaFX Library All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name ProconServerRPG JavaFX Library nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ */
+package tools.fx.load;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author takunoji
+ *
+ * 2020/04/27
+ */
+class CharctorLoaderTest {
+	/** 対象クラス */
+	private CharctorLoader target;
+
+	@Test
+	public void test() {
+		target = new CharctorLoader("");
+	}
+
+}
